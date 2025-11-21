6d21da07c2d7dca66fee7cec6386b9ccc2100494
EylonBourak <eylon.bourak@gmail.com>
2025-11-21 14:55:13 +0100

    Update cinemust.js

--- cinemust.js
+++ cinemust.js
@@ -43,4 +43,18 @@
    }
  }
 
- 
+
+import Splide from '@splidejs/splide';
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
+
+const splide = new Splide( '.splide', {
+  type   : 'loop',
+  drag   : 'free',
+  focus  : 'center',
+  perPage: 10,
+  autoScroll: {
+    speed: 1,
+  },
+} );
+
+splide.mount();
